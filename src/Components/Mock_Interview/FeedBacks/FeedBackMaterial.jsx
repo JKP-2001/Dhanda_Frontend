6a220a373e4e5dd2a86820ab1ffdcb0d29c18af2
Jitendra Kumar Pandey <84286157+JKP-2001@users.noreply.github.com>
2023-12-21 14:09:14 +0530

    Added Interviewer Profile Section And Add Some Changes

--- src/Components/Mock_Interview/FeedBacks/FeedBackMaterial.jsx
+++ src/Components/Mock_Interview/FeedBacks/FeedBackMaterial.jsx
@@ -1,31 +1,30 @@
 import React from 'react'
 
-const FeedBackMaterial = () => {
-  return (
-    <>
+const FeedBackMaterial = (props) => {
+
+    const { name, text } = props
+
+    return (
+        <>
             <div className='flex '>
                 <div className='logo'>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/NITT_logo.png" alt="logo" className='h-[40px] w-[40px] mt-5 mx-4' />
+                    <img src="https://cdn-icons-png.flaticon.com/512/6378/6378141.png" alt="logo" className='h-[40px] w-[40px] mt-5 mx-4' />
                 </div>
                 <div className="info">
-                    <h1 className="font-roboto text-base font-medium mt-6 ">Amar Sen</h1>
+                    <h1 className="font-roboto text-base font-medium mt-6 ">{name}</h1>
                 </div>
 
             </div>
             <div className="about pb-5">
                 <h1 className="font-inter text-sm text-gray-500 ml-[70px] md:ml-[70px] mx-10 break-words">
-                    Filler text is text that shares some characteristics of a real written text, but is
-                    random or otherwise generated. It may be used to display a sample of fonts, 
-                    generate text for testing, or to spoof an e-mail spam filter.
-
-                    
+                    {text}
                 </h1>
             </div>
 
-            
-            
+
+
         </>
-  )
+    )
 }
 
 export default FeedBackMaterial
