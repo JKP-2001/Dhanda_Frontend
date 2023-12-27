1d698ce9e82df27c0c633f9fe3ec2a521841e197
Jitendra Kumar Pandey <84286157+JKP-2001@users.noreply.github.com>
2023-12-27 11:21:28 +0530

    Added SignIn and SignUp Pages

--- src/Utils/functions.jsx
+++ src/Utils/functions.jsx
@@ -2,3 +2,30 @@
 export const scrollToTop = () => {
     window.scrollTo(0, 0);
 };
+
+
+export const isValidEmail = (email) => {
+    
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    
+    return emailRegex.test(email);
+};
+
+
+export const isStrongPassword = (password) => {
+    
+    const hasUpperCase = /[A-Z]/.test(password);
+
+    
+    const hasSymbol = /[$&+,:;=?@#|'<>.^*()%!-]/.test(password);
+
+    
+    const hasNumber = /\d/.test(password);
+
+    
+    const hasMinLength = password.length >= 8;
+
+    
+    return hasUpperCase && hasSymbol && hasNumber && hasMinLength;
+};
