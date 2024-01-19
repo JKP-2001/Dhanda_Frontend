b136057856f5c99d334aa6de2f7785d322a937a8
Jitendra Kumar Pandey <84286157+JKP-2001@users.noreply.github.com>
2024-01-19 23:23:15 +0530

    Added like and share modal - 2

--- src/Components/New_Feeds/Small_Profile_Card.jsx
+++ src/Components/New_Feeds/Small_Profile_Card.jsx
@@ -10,7 +10,7 @@ const Small_Profile_Card = () => {
             <div className="flex leftside space-x-2">
                 <img id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" className={`w-12 h-12 rounded-full cursor-pointer`} src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="User dropdown" onClick={() => { navigate("/user/profile/:user")}} />
 
-                <div className="flex flex-col mt-1">
+                <div className=" mt-1">
                     <p className="text-sm font-bold font-inter">User Name</p>
                     <p className="text-sm text-gray-500 font-inter">User Email</p>
                 </div>
