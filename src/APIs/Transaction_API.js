d802abb8affea7980ed56dffe423d023d20cbdd2
Jitendra Kumar Pandey <84286157+JKP-2001@users.noreply.github.com>
2024-03-30 16:15:38 +0530

    Added CSV download for transactions

--- src/APIs/Transaction_API.js
+++ src/APIs/Transaction_API.js
@@ -8,11 +8,11 @@ const security_key = process.env.REACT_APP_SECURITY_KEY;
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 
-export const fetchTransactions = async (page, limit) => {
+export const fetchTransactions = async (page, limit, month) => {
 
     try {
 
-        const response = await fetch(BASE_URL + "/transactions"+`?page=${page}&limit=${limit}`, {
+        const response = await fetch(BASE_URL + "/transactions"+`?page=${page}&limit=${limit}&month=${month}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -29,4 +29,28 @@ export const fetchTransactions = async (page, limit) => {
 
         return { success: false, msg: err.toString() }
     }
+}
+
+export const exportTransactions = async (month) => {
+
+    try {
+
+        const response = await fetch(BASE_URL + `/transactions/export?month=${month}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "security-key": security_key,
+                "auth-token": localStorage.getItem("token"),
+            },
+        });
+
+        const json = await response.json();
+
+        return json;
+
+    } catch (err) {
+
+        return { success: false, msg: err.toString() }
+    }
+
 }
