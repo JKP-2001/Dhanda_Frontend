6b19d9cccfd5fb7bd383c919b08cd00853c05848
Jitendra Kumar Pandey <84286157+JKP-2001@users.noreply.github.com>
2024-04-04 13:39:59 +0530

    Update AchieveGoal.js

--- src/Components/HomeComponents/AchieveGoal.js
+++ src/Components/HomeComponents/AchieveGoal.js
@@ -7,8 +7,8 @@ function AchieveGoal() {
       <div className="flex lg:flex-row flex-col w-[96%] md:w-[80%] item-center justify-between mx-auto gap-12 lg:gap-10 bg-[#002147] p-6 lg:p-4 rounded-lg">
         <img className="h-auto w-[321px] mx-auto lg:w-[40%] rounded-3xl" src={achiveCard}></img>
         <div className="font-handwritten2 text-2xl lg:text-4xl   lg:mt-[11rem] text-white text-center lg:text-left lg:w-[60%]">
-          Transform Your Skills with top companies expert mentorship and
-          realistic mock interviews
+          {"Transform your skills with top companies expert's mentorship and
+          realistic mock interviews."}
         </div>
       </div>
     </div>
