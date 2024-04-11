b011f71fc83dd4e50c1ab9694e4c0dea71e941af
Jitendra Kumar Pandey <84286157+JKP-2001@users.noreply.github.com>
2024-04-11 18:15:16 +0530

    Optimize Inital Loading 2

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
 import "./App.css";
-import { lazy, Suspense } from "react";
 
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  useNavigate,
 } from "react-router-dom";
 
+import MockInterviewProfile from "./Pages/Mock_Interviewer/MockInterviewProfile";
+import Home from "./Pages/Home";
+
+import Mock_Interview from "./Pages/Mock_Interview/Mock_Interview";
+import NotFound from "./Pages/NotFound";
+import Interviewer_Card from "./Components/Mock_Interview/Interviewer_Card";
+import SignUp from "./Pages/Auth/SignUp";
+import { Toaster } from "react-hot-toast";
+import ForgotPassword from "./Pages/Auth/ForgotPassword";
+import NewPassword from "./Pages/Auth/NewPassword";
+import SignIn from "./Pages/Auth/SignIn";
+import Calendar_Page from "./Pages/Schedule_Interview/Calendar_Page";
+import User_Profile from "./Pages/Profile/User_Profile";
+
 import ProtectedRoute from "./Utils/ProtectedRoute";
+
 import { getUserData } from "./APIs/User_API";
 import showToast from "./Utils/showToast";
 import { decryptFromJson } from "./Utils/functions";
 import { getUserSuccess } from "./Redux/user/userSlice";
 import { useDispatch } from "react-redux";
-import { Toaster } from "react-hot-toast";
-
-
-import Home from "./Pages/Home";
-import NotFound from "./Pages/NotFound";
 import GoogleAuthCallBack from "./Pages/Auth/GoogleAuthCallBack";
-import Loader from "./Utils/Loader";
+import SinglePost from "./Pages/SinglePost";
 
-const SignUp = lazy(() => import("./Pages/Auth/SignUp"));
-const ForgotPassword = lazy(() => import("./Pages/Auth/ForgotPassword"));
-const NewPassword = lazy(() => import("./Pages/Auth/NewPassword"));
-const SignIn = lazy(() => import("./Pages/Auth/SignIn"));
-const Calendar_Page = lazy(() => import("./Pages/Schedule_Interview/Calendar_Page"));
-const User_Profile = lazy(() => import("./Pages/Profile/User_Profile"));
-const PaymentPage = lazy(() => import("./Pages/Payment/PaymentPage"));
-const BookingPage = lazy(() => import("./Pages/Booking/BookingPage.jsx"));
-const DM = lazy(() => import("./Pages/Profile/DM"));
-const TimeSlotPage = lazy(() => import("./Pages/Profile/TimeSlotPage.jsx"));
-const Contact = lazy(() => import("./Pages/Contact"));
-const MockInterviewProfile = lazy(() => import("./Pages/Mock_Interviewer/MockInterviewProfile.jsx"));
-const Mock_Interview = lazy(() => import("./Pages/Mock_Interview/Mock_Interview.jsx"));
-const Interviewer_Card = lazy(() => import("./Components/Mock_Interview/Interviewer_Card.jsx"));
-
-
+import DiscussionPostDetails from "./Pages/Discussion/DiscussionPostDetails/DiscussionPostDetails";
+import PaymentPage from "./Pages/Payment/PaymentPage";
+import BookingPage from "./Pages/Booking/BookingPage";
+import DM from "./Pages/Profile/DM";
+import TimeSlotPage from "./Pages/Profile/TimeSlotPage";
+import Contact from "./Pages/Contact";
 
 
 export const getLoginUser = async (dispatch, navigate) => {
@@ -99,142 +99,67 @@ const App = () => {
       <Toaster />
       <Router>
         <Routes>
-
-          <Route path="/signin" element={
-            <Suspense fallback={<Loader />}>
-              <SignIn />
-            </Suspense>} />
-
-          <Route path="/contact-us" element={
-            <Suspense fallback={<Loader />}>
-              <Contact />
-            </Suspense>} />
-
-          <Route path="/signup" element={
-            <Suspense fallback={<Loader />}>
-              <SignUp />
-            </Suspense>} />
-
-
-          <Route path="/forgot-password" element={
-            <Suspense fallback={<Loader />}>
-              <ForgotPassword />
-            </Suspense>} />
-
-          <Route path="/reset-password" element={
-            <Suspense fallback={<Loader />}>
-              <NewPassword />
-            </Suspense>} />
-
-          <Route path="/" element={
-            <Suspense fallback={<Loader />}>
-              <ProtectedRoute ele={<Home />} />
-            </Suspense>} />
-
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/contact-us" element={<Contact />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<NewPassword />} />
+          <Route path="/" element={<ProtectedRoute ele={<Home />} />} />
           <Route
             path="/mock-interview"
-            element={
-              <Suspense fallback={<Loader />}>
-                <ProtectedRoute ele={<Mock_Interview />} />
-              </Suspense>
-            }
+            element={<ProtectedRoute ele={<Mock_Interview />} />}
           />
-
           <Route
             path="/mock-interview/:role/:user_id"
-            element={
-              <Suspense fallback={<Loader />}>
-                <ProtectedRoute ele={<MockInterviewProfile />} />
-              </Suspense>
-            }
+            element={<ProtectedRoute ele={<MockInterviewProfile />} />}
           />
-
-
           <Route
             path="/payment"
-            element={
-              <Suspense fallback={<Loader />}>
-                <ProtectedRoute ele={<PaymentPage />} />
-              </Suspense>}
+            element={<ProtectedRoute ele={<PaymentPage />} />}
           />
-
-
-          <Route path="/dm" element={
-            <Suspense fallback={<Loader />}>
-              <ProtectedRoute ele={<DM />} />
-            </Suspense>} />
-
-
+          <Route path="/dm" element={<ProtectedRoute ele={<DM />} />} />
           <Route
             path="/bookings"
-            element={<Suspense fallback={<Loader />}>
-              <ProtectedRoute ele={<BookingPage />} />
-            </Suspense>}
+            element={<ProtectedRoute ele={<BookingPage />} />}
           />
-
-
           <Route
             path="/time-slots"
-            element={
-              <Suspense fallback={<Loader />}>
-                <ProtectedRoute ele={<TimeSlotPage />} />
-              </Suspense>}
+            element={<ProtectedRoute ele={<TimeSlotPage />} />}
           />
-
           <Route
             path="/test"
-            element={<Suspense fallback={<Loader />}>
-              <ProtectedRoute ele={<Interviewer_Card />} />
-            </Suspense>}
-          />
-
-
+            element={<ProtectedRoute ele={<Interviewer_Card />} />}
+          />{" "}
           {/* Use to test component */}
           <Route path="*" element={<NotFound />} />
-
           <Route
             path="/mock-interview/schedule/:user"
-            element={<Suspense fallback={<Loader />}>
-              <ProtectedRoute ele={<Calendar_Page />} />
-            </Suspense>}
+            element={<ProtectedRoute ele={<Calendar_Page />} />}
           />
-
           <Route
             path="/user/profile/:role/:id"
-            element={
-              <Suspense fallback={<Loader />}>
-                <ProtectedRoute ele={<User_Profile />} />
-              </Suspense>
-            }
+            element={<ProtectedRoute ele={<User_Profile />} />}
           />
-
-          <Route
-            path="/google/auth/callback"
-            element={
-              <Suspense fallback={<Loader />}>
-                <GoogleAuthCallBack />
-              </Suspense>
-            }
-          />
-
-
           {/* <Route
             path="/new-feeds"
             element={<ProtectedRoute ele={<New_Feeds />} />}
           /> */}
-          {/* <Route
+          <Route
             path="/discuss/:discussId"
             element={<ProtectedRoute ele={<DiscussionPostDetails />} />}
-          /> */}
-          {/* <Route
+          />
+          <Route
             path="/post/:id"
             element={<ProtectedRoute ele={<SinglePost />} />}
-          /> */}
+          />
           {/* <Route
             path="/discuss"
             element={<ProtectedRoute ele={<Discussion />} />}
           /> */}
-
+          <Route
+            path="/google/auth/callback"
+            element={<GoogleAuthCallBack />}
+          />
         </Routes>
       </Router>
     </>
