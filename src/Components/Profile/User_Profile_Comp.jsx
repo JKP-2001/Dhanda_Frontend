5600081482b902335c1c7d1e184a3304d011a6e3
Jitendra Kumar Pandey <84286157+JKP-2001@users.noreply.github.com>
2024-04-13 12:17:48 +0530

    edit personal info feature 2

--- src/Components/Profile/User_Profile_Comp.jsx
+++ src/Components/Profile/User_Profile_Comp.jsx
@@ -833,12 +833,12 @@ const User_Profile_Comp = () => {
                 type="text"
                 name="repeat_password"
                 id="floating_repeat_password"
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer resize-none"
+                className="block py-2.5 px-0 w-full sm:text-sm text-gray-900 bg-transparent border-0 border-gray-300 appearance-none dark:text-white text-xs  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer resize-none"
                 placeholder=" "
                 required
                 disabled
                 value={seeMore ? userRedux.data.description : userRedux.data.description.slice(0, 400)}
-                rows={Math.max(5, Math.ceil(userRedux.data.description.length / 100))} // Adjust the divisor as needed
+                rows={Math.max(8, Math.ceil(userRedux.data.description.length / 100))} // Adjust the divisor as needed
               />
 
 
